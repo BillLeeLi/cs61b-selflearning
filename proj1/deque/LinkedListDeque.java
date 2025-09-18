084f46aa367aa221b3b86733866e8e7033ae7f7f
BillLeeLi <321689251@qq.com>
2025-09-18 18:27:34 +0800

    First version of LinkedListDeque.java. We haven't defined class Iterator yet. Fortunately, we have passed all the basic tests in LinkedListDequeTest.java!

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,5 +1,128 @@
 package deque;
 
-public class LinkedListDeque {
+public class LinkedListDeque<T> {
+    // 采用循环链表、双向链接的实现方式
+    private class Node {
+        public T item;
+        public Node next;  // 下一个节点
+        public Node prev;  // 上一个节点
+        public Node(T i, Node n, Node p) {
+            item = i;
+            next = n;
+            prev = p;
+        }
+    }
 
+    private Node sentinel;
+    private int size;
+
+    public LinkedListDeque() {
+        sentinel = new Node((T) new Object(), null, null);  // 注意这时sentinel是null，所以不能在构造函数中把prev和next设为sentinel
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+    }
+
+    public LinkedListDeque(T first) {
+        Node firstNode = new Node(first, null, null);
+        sentinel = new Node((T) new Object(), firstNode, firstNode);
+        firstNode.prev = firstNode.next = sentinel;
+        size = 1;
+    }
+
+    public void addFirst(T item) {
+        Node newNode = new Node(item, sentinel.next, sentinel);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size++;
+    }
+
+    public void addLast(T item) {
+        Node newNode = new Node(item, sentinel, sentinel.prev);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        Node n = sentinel.next;
+        while (n != sentinel) {
+            System.out.print(n.item + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        Node discardedNode = sentinel.next;
+        sentinel.next = discardedNode.next;
+        sentinel.next.prev = sentinel;
+        size--;
+        return discardedNode.item;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        Node discardedNode = sentinel.prev;
+        sentinel.prev = discardedNode.prev;
+        sentinel.prev.next = sentinel;
+        size--;
+        return discardedNode.item;
+    }
+    public T get(int index) {
+        if (size <= index) {
+            return null;
+        }
+
+        Node n;
+        if (index < size / 2) {
+            // 正向查找
+            n = sentinel.next;
+            for (int i = 0; i < index; i++) {
+                n = n.next;
+            }
+            return n.item;
+        }
+        else {
+            n = sentinel.prev;
+            for (int i = size - 1; i > index; i++) {
+                n = n.prev;
+            }
+        }
+        return n.item;
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof LinkedListDeque) {
+            if (size != ((LinkedListDeque<?>) o).size) {
+                return false;
+            }
+
+            Node n1 = sentinel.next;
+            Node n2 = (Node) ((LinkedListDeque<?>) o).sentinel.next;
+            while (n1 != sentinel) {
+                if (!n1.item.equals(n2)) {
+                    return false;
+                }
+                n1 = n1.next;
+                n2 = n2.next;
+            }
+            return true;
+        }
+        return false;
+    }
+//    public Iterator<T> iterator();
 }
