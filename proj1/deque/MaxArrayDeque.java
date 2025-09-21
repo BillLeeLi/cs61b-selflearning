086c788024190cf3f59e6fc929c6d32c3a435d5a
BillLeeLi <321689251@qq.com>
2025-09-21 10:38:34 +0800

    Add MaxArrayDeque.java

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,38 @@
+package deque;
+
+import javax.crypto.MacSpi;
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    Comparator<T> comp;
+    public MaxArrayDeque(Comparator<T> c) {
+        super();
+        comp = c;
+    }
+
+    public T max() {
+        if (isEmpty()) {
+            return null;
+        }
+        int maxIndex = 0;
+        for (int i =0; i < size(); i++) {
+            if (comp.compare(get(maxIndex), get(i)) < 0) {
+                maxIndex = i;
+            }
+        }
+        return get(maxIndex);
+    }
+
+    public T max(Comparator<T> c) {
+        if (isEmpty()) {
+            return null;
+        }
+        int maxIndex = 0;
+        for (int i =0; i < size(); i++) {
+            if (c.compare(get(maxIndex), get(i)) < 0) {
+                maxIndex = i;
+            }
+        }
+        return get(maxIndex);
+    }
+}
