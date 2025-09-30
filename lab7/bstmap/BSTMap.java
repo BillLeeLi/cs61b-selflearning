62af5eb2aac8ef20172c9686a25390917edb8e76
BillLeeLi <321689251@qq.com>
2025-09-30 22:18:55 +0800

    实现了remove,iterator和keySet方法，通过了 TestBSTMapExtra.java中的测试。但是测试中似乎没有测试过迭代器的正确性，所以我自己编写了一个简单的样例测试了迭代器是正常运行的。

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,7 +1,9 @@
 package bstmap;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.HashSet;
 
 public class BSTMap<KType extends Comparable<KType>, VType> implements Map61B<KType, VType> {
     private Node<KType, VType> root;
@@ -72,6 +74,10 @@ public class BSTMap<KType extends Comparable<KType>, VType> implements Map61B<KT
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(KType key, VType value) {
+        if (key == null) {
+            throw new NoSuchElementException("put null pointer");
+        }
+
         root = put(key, value, root);
     }
 
@@ -84,6 +90,8 @@ public class BSTMap<KType extends Comparable<KType>, VType> implements Map61B<KT
                 r.right = put(key, value, r.right);
             } else if (cmp > 0) {
                 r.left = put(key, value, r.left);
+            } else {
+                r.val = value;
             }
         }
         r.size = size(r.left) + size(r.right) + 1;  // 注意插入有可能失败，所以不可以直接r.size++
@@ -94,7 +102,19 @@ public class BSTMap<KType extends Comparable<KType>, VType> implements Map61B<KT
      * If you don't implement this, throw an UnsupportedOperationException. */
     @Override
     public Set<KType> keySet() {
-        throw new UnsupportedOperationException("keySet() not implemented");
+        Set<KType> res = new HashSet<>();
+        keySetHelper(root, res);
+        return res;
+    }
+
+    private void keySetHelper(Node<KType, VType> r, Set<KType> s) {
+        if (r == null) {
+            return;
+        }
+
+        s.add(r.key);
+        keySetHelper(r.left, s);
+        keySetHelper(r.right, s);
     }
 
     /* Removes the mapping for the specified key from this map if present.
@@ -102,7 +122,64 @@ public class BSTMap<KType extends Comparable<KType>, VType> implements Map61B<KT
      * UnsupportedOperationException. */
     @Override
     public VType remove(KType key) {
-        throw new UnsupportedOperationException("keySet() not implemented");
+        if (key == null) {
+            throw new NoSuchElementException("remove null pointer");
+        }
+
+        VType res = get(key);  // 获取返回值
+        root = remove(key, root);  // 删除节点
+        return res;
+    }
+
+    private Node<KType, VType> remove(KType key, Node<KType, VType> r) {
+        if (r == null) {  // 没有找到需要删除的元素
+            return null;
+        }
+
+        int cmp = r.key.compareTo(key);
+        if (cmp < 0) {
+            r.right = remove(key, r.right);
+        } else if (cmp > 0) {
+            r.left = remove(key, r.left);
+        } else {  // 要删除当前节点r
+            if (r.left == null) {
+                return r.right;
+            } else if (r.right == null) {
+                return r.left;
+            } else {
+                Node<KType, VType> t = r;
+                r = getMin(t.right);
+                r.right = removeMin(t.right);
+                r.left = t.left;
+            }
+        }
+        r.size = size(r.left) + size(r.right) + 1;
+        return r;
+    }
+
+    private Node<KType, VType> getMin(Node<KType, VType> r) {
+        if (r == null) {
+            throw new NoSuchElementException("getMin() on empty tree");
+        }
+
+        Node<KType, VType> curr = r;
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr;
+    }
+
+    private Node<KType, VType> removeMin(Node<KType, VType> r) {
+        if (r == null) {
+            throw new NoSuchElementException("removeMin() on empty tree");
+        }
+
+        if (r.left == null) {
+            return r.right;
+        }
+        r.left = removeMin(r.left);
+        r.size = size(r.left) + size(r.right) + 1;
+        return r;
     }
 
     /* Removes the entry for the specified key only if it is currently mapped to
@@ -110,12 +187,52 @@ public class BSTMap<KType extends Comparable<KType>, VType> implements Map61B<KT
      * throw an UnsupportedOperationException.*/
     @Override
     public VType remove(KType key, VType value) {
-        throw new UnsupportedOperationException("keySet() not implemented");
+        if (key == null) {
+            throw new NoSuchElementException("remove null pointer");
+        }
+
+        if (value != null && value.equals(get(key))) {
+            return remove(key);
+        }
+        return null;
     }
 
     @Override
     public Iterator<KType> iterator() {
-        throw new UnsupportedOperationException("keySet() not implemented");
+        return new BSTMapIterator();
+    }
+
+    public class BSTMapIterator implements Iterator<KType> {
+        int index;
+        public BSTMapIterator() {
+            index = 0;
+        }
+
+        public boolean hasNext() {
+            return index < size();
+        }
+
+        public KType next() {
+            return getIndex(index++, root);
+        }
+    }
+
+    private KType getIndex(int index, Node<KType, VType> r) {
+        if (r == null) {
+            throw new NoSuchElementException("getIndex on empty tree");
+        } else if (index >= r.size) {
+            throw new NoSuchElementException("getIndex: index should be smaller than tree size");
+        } else if (index < 0) {
+            throw new NoSuchElementException("getIndex: index should be larger than or equal to zero");
+        }
+
+        if (index < size(r.left)) {
+            return getIndex(index, r.left);
+        } else if (index > size(r.left)) {
+            return getIndex(index - size(r.left) - 1, r.right);
+        } else {
+            return r.key;
+        }
     }
 
     public void printInOrder() {
@@ -155,5 +272,8 @@ public class BSTMap<KType extends Comparable<KType>, VType> implements Map61B<KT
         m.put("apple", 30);
         m.put("watermelon", 15);
         m.printInOrder();
+        for (String s: m) {
+            System.out.println(s);
+        }
     }
 }
